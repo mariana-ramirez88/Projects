7748dc968e5ea581f333b8faa632468997813ccd
Mariana <mariram2002@gmail.com>
2022-12-26 11:50:47 -0500

    clase 03 completada

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const btn = document.querySelector("[data-form-btn]");
-console.log(btn);
 
 const  createTask = (evento) => {  
-    evento.preventDefault()
+    evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+    const value = input.value;
+    const list = document.querySelector("[data-list]")
+    const task = document.createElement('li');
+    task.classList.add("card")
+    input.value = "";
+    // con backticks para template strings permite combinar java y html
+    const content = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    task.innerHTML = content;
+    list.appendChild(task);
+    console.log(content);
 
-}
+};
+
+console.log(btn);
+
 // arrow fnuction o funciones anonimas
 btn.addEventListener("click", createTask);
 
