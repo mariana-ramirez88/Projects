1807774dbf7ee049e9c59db499c61c4dcbd81406
Mariana <mariram2002@gmail.com>
2023-01-12 21:36:09 -0500

    cambios de almacenamiento para recargar pagina

--- script.js
+++ script.js
@@ -1,44 +1,8 @@
-( () => { const btn = document.querySelector("[data-form-btn]");
+import { addTask } from "./components/addTask.js";
+import { displayTasks } from "./components/readTasks.js";
 
-const  createTask = (evento) => {  
-    evento.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    const value = input.value;
-    const list = document.querySelector("[data-list]")
-    const task = document.createElement('li');
-    task.classList.add("card")
-    input.value = "";
-    // con backticks para template strings permite combinar java y html
-    const taskContent = document.createElement('div');
-    taskContent.appendChild(checkComplete());
-    const titleTask = document.createElement('span');
-    titleTask.classList.add('task');
-    titleTask.innerText = value;
-    taskContent.appendChild(titleTask)
-    const content = 
-    `<i class="fas fa-trash-alt trashIcon icon"></i>`;
-   // task.innerHTML = content;
-   task.appendChild(taskContent);
-    list.appendChild(task);
-
-};
-
+const btn = document.querySelector("[data-form-btn]");
 
 // arrow fnuction o funciones anonimas
-btn.addEventListener("click", createTask);
-
-const checkComplete = () => {
-    const i = document.createElement('i');
-    i.classList.add("far","fa-check-square","icon" );
-    i.addEventListener('click', completeTask);
-    return i
-};
-
-const completeTask = (event) => {
-    const element = event.target;
-    element.classList.toggle('fas');
-    element.classList.toggle('completeIcon');
-    element.classList.toggle('far');
-
-};
-})();
+btn.addEventListener("click", addTask);
+displayTasks();
